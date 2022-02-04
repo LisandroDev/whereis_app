f5bb550e0afda16878c21a0238e04622adecd960
lichaa824 <lisandrorubi@gmail.com>
2022-02-04 10:33:07 -0300

    Buttons fully functional

--- src/Components/ContextMenu.jsx
+++ src/Components/ContextMenu.jsx
@@ -1,6 +1,6 @@
 import "../App.css";
 function ContextMenu(props) {
-  const styles = {
+  const DivStyle = {
     top: props.y - 75,
     left: props.x - 75,
     zIndex: isViewable(),
@@ -15,10 +15,33 @@ function ContextMenu(props) {
   }
 
   return (
-    <div style={styles} className="context-menu">
-      <button className="button-contextmenu" onClick={() => console.log("perro")}>Turtle</button>
-      <button className="button-contextmenu">Bee Reading</button>
-      <button className="button-contextmenu">Flying squirrel</button>
+    <div style={DivStyle} className="context-menu">
+      <button
+        style={{ pointerEvents: props.characterFound.turtle ? "none" : "auto" }}
+        className="button-contextmenu"
+        value={"turtle"}
+        onClick={(e) => props.handleOptionChoose(e.target.value)}
+      >
+        Turtle
+      </button>
+      <button
+        style={{ pointerEvents: props.characterFound.bee ? "none" : "auto" }}
+        className="button-contextmenu"
+        value={"bee"}
+        onClick={(e) => props.handleOptionChoose(e.target.value)}
+      >
+        Bee Reading
+      </button>
+      <button
+        style={{
+          pointerEvents: props.characterFound.squirrel ? "none" : "auto",
+        }}
+        className="button-contextmenu"
+        value={"squirrel"}
+        onClick={(e) => props.handleOptionChoose(e.target.value)}
+      >
+        Flying squirrel
+      </button>
     </div>
   );
 }
